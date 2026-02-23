9a09b8f07cf89b8594051cf04b81cead7394ddb5
KUK <khushikulkarni55@gmail.com>
2026-02-23 17:04:20 +0530

    first commit

---
+++ src/c2c/sr_developer/KIA_Internal.java
@@ -0,0 +1,14 @@
+package c2c.sr_developer;
+
+import c2c.Jr_Developer.KIA_AirBags;
+
+public class KIA_Internal extends KIA_AirBags {
+
+	public static void main(String[] args) {
+		
+		KIA_Internal ob=new KIA_Internal();
+		ob.display_Air();
+		
+	}
+
+}
