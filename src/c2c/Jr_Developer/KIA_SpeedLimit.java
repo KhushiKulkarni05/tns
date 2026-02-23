df37c1b8ba4ef35d3e643b1b4ce12f36f0adc003
KUK <khushikulkarni55@gmail.com>
2026-02-23 16:14:42 +0530

    first commit

---
+++ src/c2c/Jr_Developer/KIA_SpeedLimit.java
@@ -0,0 +1,17 @@
+package c2c.Jr_Developer;
+
+public class KIA_SpeedLimit {
+	public void display_speed()
+	{
+		
+		System.out.println("All KIA have Speed Limit");
+		
+	}
+	 
+	public static void main(String[] args) {
+		  
+		KIA_SpeedLimit ob2=new KIA_SpeedLimit();
+		ob2.display_speed();
+		
+	  }
+}
