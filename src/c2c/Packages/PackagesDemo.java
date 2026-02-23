dc8794ff3a88828154fb225203b35d35ee8af67b
KUK <khushikulkarni55@gmail.com>
2026-02-23 16:39:43 +0530

    first commit

---
+++ src/c2c/Packages/PackagesDemo.java
@@ -0,0 +1,16 @@
+package c2c.Packages;
+
+public class PackagesDemo {
+
+	public static void main(String[] args) {
+		
+		String greeting="hello world";
+		System.out.println("Original String: "  +greeting);
+		System.out.println("Upper Case String: " + greeting.toUpperCase());
+		
+		int max=Math.max(10,20);
+		System.out.println("Max: " + max);
+		
+	}
+
+}
